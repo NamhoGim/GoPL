334f82ffd6d8de5b1714edf5b2cb04fa1d33a237
Namho Kim <kkjknh2@gmail.com>
2020-05-09 21:10:13 +0900

    checkpoint till 8.5

--- ch8/netcat3/main.go
+++ ch8/netcat3/main.go
@@ -13,9 +13,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	go mustCopy(os.Stdout, conn)
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
 	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // wait for backgraound goroutine to finish
 }
 
 func mustCopy(dst io.Writer, src io.Reader) {
