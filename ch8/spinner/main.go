3bfdaa5f78631c32ceb43a428c3fad70f2ed8f63
Namho Kim <namho@molocoads.com>
2020-05-09 11:46:35 +0900

    check-in some examples

---
+++ ch8/spinner/main.go
@@ -0,0 +1,33 @@
+/*
+f() 	// call f(); wait for it to return
+go f() 	// create a new goroutine that calls f(); don't wait
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
